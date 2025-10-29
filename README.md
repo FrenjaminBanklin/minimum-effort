85ba22695367c64ec0688fe020e4dd5c066c3476
Brandon Stull <bstull@ucf.edu>
2025-10-29 08:45:51 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+This is a goofy little two-phrase ad lib generator made as a joke to accompany an equally goofy Halloween costume.
+
+Max Power is the Techrangers' robot mascot.
+
+His nephew is Minimum Effort. There are many disparaging ways of describing him. That's the joke.
---
+++ docs/index.js
@@ -0,0 +1,28 @@
+const list1 = [
+    'Dumb',
+    'Sucky',
+    'Doofus',
+    'Foolish',
+    'Simpleminded',
+    'Lazy',
+    'Dull',   
+]
+
+const list2= [
+    'College Dropout',
+    'Backup Dancer',
+    'Brainrotted',
+    'Less Popular',
+    'Illegitimate',
+    'Idiotic',
+    'Moronic'
+]
+
+document.onreadystatechange = () => {
+    if (document.readyState === "interactive") {
+        const word1 = list1[Math.floor(Math.random() * list1.length)]
+        const word2 = list2[Math.floor(Math.random() * list2.length)]
+        document.getElementById('space1').innerHTML = word1
+        document.getElementById('space2').innerHTML = word2
+    }
+}
