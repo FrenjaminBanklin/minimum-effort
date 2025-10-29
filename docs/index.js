80a30b4f8b8638a0169c53013d06143fa96b042c
Brandon Stull <bstull@ucf.edu>
2025-10-29 09:12:45 -0400

    Made it look better, added some phrases.

--- docs/index.js
+++ docs/index.js
@@ -1,28 +1,32 @@
 const list1 = [
-    'Dumb',
-    'Sucky',
-    'Doofus',
-    'Foolish',
-    'Simpleminded',
-    'Lazy',
-    'Dull',   
+	'Dumb',
+	'Sucky',
+	'Doofus',
+	'Foolish',
+	'Simpleminded',
+	'Lazy',
+	'Dull',
+	'Surly',
+	'Lame'
 ]
 
 const list2= [
-    'College Dropout',
-    'Backup Dancer',
-    'Brainrotted',
-    'Less Popular',
-    'Illegitimate',
-    'Idiotic',
-    'Moronic'
+	'College Dropout',
+	'Backup Dancer',
+	'Brainrotted',
+	'Less Popular',
+	'Illegitimate',
+	'Idiotic',
+	'Moronic',
+	'Poorly Constructed',
+	'Aftermarket'
 ]
 
 document.onreadystatechange = () => {
-    if (document.readyState === "interactive") {
-        const word1 = list1[Math.floor(Math.random() * list1.length)]
-        const word2 = list2[Math.floor(Math.random() * list2.length)]
-        document.getElementById('space1').innerHTML = word1
-        document.getElementById('space2').innerHTML = word2
-    }
+	if (document.readyState === "interactive") {
+		const word1 = list1[Math.floor(Math.random() * list1.length)]
+		const word2 = list2[Math.floor(Math.random() * list2.length)]
+		document.getElementById('space1').innerHTML = word1
+		document.getElementById('space2').innerHTML = word2
+	}
 }
