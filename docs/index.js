85be7a8b6bc8525a14fb559a12005d6a63dbadee
Brandon Stull <bstull@ucf.edu>
2025-10-29 09:25:29 -0400

    More phrases.

--- docs/index.js
+++ docs/index.js
@@ -10,7 +10,9 @@ const list1 = [
 	'lame',
 	'underclocked',
 	'rickety',
-	'ignorant'
+	'ignorant',
+	'irrelevant',
+	'gullible'
 ]
 
 const list2= [
@@ -19,17 +21,19 @@ const list2= [
 	'brainrotted',
 	'less popular',
 	'illegitimate',
-	'idiotic',
-	'moronic',
+	'idiot',
+	'moron',
 	'poorly constructed',
 	'aftermarket',
 	'rusted out',
 	'antique',
-	'scumbag'
+	'scumbag',
+	'jerry-rigged',
+	'dumpster-diving'
 ]
 
 document.onreadystatechange = () => {
-	if (document.readyState === "interactive") {
+	if (document.readyState === 'interactive') {
 		const word1 = list1[Math.floor(Math.random() * list1.length)]
 		const word2 = list2[Math.floor(Math.random() * list2.length)]
 		document.getElementById('space1').innerHTML = word1
